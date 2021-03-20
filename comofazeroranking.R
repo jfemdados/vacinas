c88f6673d355f2f664ef8a92c995b318046168cf
Marcello Filgueiras <53457944+marcellofilgueiras@users.noreply.github.com>
2021-03-20 13:59:46 -0300

    Update comofazeroranking.R

--- comofazeroranking.R
+++ comofazeroranking.R
@@ -6,7 +6,7 @@ vacinometro <- rio::import("vacina/mapa_vacina.xlsx")%>%
          !Eficiencia > 100)%>%
   arrange(desc(Eficiencia))%>%
   filter(Nome_do_municipio %in% c("JUIZ DE FORA", "ALÉM PARAÍBA" , "UBÁ" , "MURIAÉ",
-                                  "CATAGUASES", "SANTOS DUMONT", "VIÇOSA", "LEOPOLDINA", "CARANGOLA")
+                                  "CATAGUASES", "SANTOS DUMONT", "VIÇOSA", "LEOPOLDINA", "CARANGOLA"))
 
 export(vacinometro, "vacina/flourish-classificacaovacina.csv")
 
