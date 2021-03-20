bbbf82d1de33c2f5b1f1fcd7eaaab330add78cb2
Marcello Filgueiras <53457944+marcellofilgueiras@users.noreply.github.com>
2021-03-20 13:59:28 -0300

    Update comofazeroranking.R
    diminuindo o codigo com %in%

--- comofazeroranking.R
+++ comofazeroranking.R
@@ -5,9 +5,8 @@ vacinometro <- rio::import("vacina/mapa_vacina.xlsx")%>%
   filter(Nome_da_meso  == "Zona da Mata",
          !Eficiencia > 100)%>%
   arrange(desc(Eficiencia))%>%
-  filter(Nome_do_municipio == "JUIZ DE FORA" | Nome_do_municipio == "ALÉM PARAÍBA" | Nome_do_municipio == "UBÁ" |
-           Nome_do_municipio == "MURIAÉ" | Nome_do_municipio == "CATAGUASES" | Nome_do_municipio == "SANTOS DUMONT"|
-           Nome_do_municipio == "VIÇOSA" | Nome_do_municipio == "LEOPOLDINA" | Nome_do_municipio == "CARANGOLA")
+  filter(Nome_do_municipio %in% c("JUIZ DE FORA", "ALÉM PARAÍBA" , "UBÁ" , "MURIAÉ",
+                                  "CATAGUASES", "SANTOS DUMONT", "VIÇOSA", "LEOPOLDINA", "CARANGOLA")
 
 export(vacinometro, "vacina/flourish-classificacaovacina.csv")
 
